6a32c5fc44d6cd99383fdb945ef865cb5aa34acc
harunkurtme <harunlakodla@gmail.com>
2023-04-02 18:18:15 +0300

    file update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-# FROM ubuntu:20.04
+FROM ubuntu:20.04
 LABEL Name=kartaca_ws Version=0.0.1
 
+RUN echo "rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'mongo-master:27017'}, {_id: 1, host: 'mongo-slave1:27018'}, {_id: 2, host: 'mongo-slave3:27019'}]})" >> /docker-entrypoint-initdb.d/mongo-init.js
--- servis_gelistirme/servis_gelistirme.py
+++ servis_gelistirme/servis_gelistirme.py
@@ -18,9 +18,15 @@ def get_token(come_data) -> str:
 
 async def hello():
     async with websockets.connect("wss://cekirdektenyetisenler.kartaca.com/ws") as websocket:
+        
+        value="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")
+        reverseValue=[]
+            
+        print(type(value.reverse()))
         sendCommand = {"type": "REGISTER", "name": "Harun KURT",
                        "email": "harunkurt0000@gmail.com", "phone": "05458146806", "password": ""}
         come_data = await websocket.recv()
+        print(json.loads(come_data)["message"])
         # print(come_data.replace('"', "").replace('}', ""))
         print(get_token(come_data=come_data))
         sendCommand["password"] = get_token(come_data=come_data)
