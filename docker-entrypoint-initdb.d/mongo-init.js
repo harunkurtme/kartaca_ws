cfa59666b59ad7e6f4fd0ca042cecfba5e8705b1
harunkurtme <harunlakodla@gmail.com>
2023-04-02 18:47:13 +0300

    uupdate mongo-cluster

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,19 +1,30 @@
-print('Start creating database ##########################')
-db = db.getSiblingDB('api_dev_db');
-db.createUser(
-    {
-        user: 'api_user',
-        pwd:  'api1234',
-        roles: [{role: 'readWrite', db: 'api_dev_db'}],
-    }
-);
+// Set the replica set name
+rs.initiate({_id: "rs0", version: 1});
 
-db = db.getSiblingDB('api_test_db');
-db.createUser(
-    {
-        user: 'api_user',
-        pwd:  'api1234',
-        roles: [{role: 'readWrite', db: 'api_test_db'}],
-    }
-);
-print('End creating database ##########################')
+// Wait for the replica set to be fully initialized
+while (rs.status().startupStatus || (rs.status().hasOwnProperty("myState") && rs.status().myState != 1)) {
+  print("Waiting for the replica set to be fully initialized...");
+  sleep(1000);
+}
+
+// Create the stajdb database
+db = db.getSiblingDB('stajdb');
+
+// Enable authentication
+db.createUser({
+  user: 'stajuser',
+  pwd: 'stajpassword',
+  roles: [{role: 'readWrite', db: 'stajdb'}]
+});
+
+// Create the "iller" collection and insert 10 documents
+db.createCollection('iller');
+for (i = 1; i <= 10; i++) {
+  db.iller.insert({il_adi: 'İl ' + i});
+}
+
+// Create the "ulkeler" collection and insert 10 documents
+db.createCollection('ulkeler');
+for (i = 1; i <= 10; i++) {
+  db.ulkeler.insert({ulke_adi: 'Ülke ' + i});
+}
